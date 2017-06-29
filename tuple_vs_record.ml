22b3372cc91aae9fe6d2a6e342afaaef813ccbda
spatters <sam.x.patterson@gmail.com>
2017-06-29 16:34:51 -0400

    more bench

--- tuple_vs_record.ml
+++ tuple_vs_record.ml
@@ -2,11 +2,24 @@ open Core
 open Core_bench
 
 let bench tests =
-  Bench.bench tests
+  let run_config = 
+    Bench.Run_config.create 
+      ~verbosity:`High 
+      ~time_quota: (Time.Span.of_sec 2.)
+      () 
+  in
+  Bench.bench ~run_config tests
 ;;
 
 let tests =
-  [ Bench.Test.create ~name:"tuple" (fun ()-> (2,3))]
+  [Bench.Test.create ~name:"tuple" 
+    (fun ()-> ignore (2.,3.))
+    (*Bench.Test.create ~name:"Time.now" 
+    (fun () -> ignore (Time.now ()));
+    Bench.Test.create ~name:"Array.create3000" 
+    (fun () -> ignore (Array.create 3000 0))*)
+  ]
+;;
 
 let () =
   bench tests
