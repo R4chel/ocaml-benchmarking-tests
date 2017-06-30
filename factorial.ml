70d0ba08f57225fff3228a10b86978de39771e29
spatters <sam.x.patterson@gmail.com>
2017-06-30 15:51:29 -0400

    .

--- factorial.ml
+++ factorial.ml
@@ -15,10 +15,14 @@ let factorial_2 n =
 ;;
 
 let factorial_3 n =
-    List.fold (List.range 1 (n+1)) ~init:1 ~f:(fun acum n -> acum * n)
+    List.fold_left (List.range 1 (n+1)) ~init:1 ~f:(fun acum n -> acum * n)
 ;;
 
 let factorial_4 n =
+    List.fold_right (List.range 1 (n+1)) ~init:1 ~f:(fun n acum -> acum * n)
+;;
+
+let factorial_5 n =
   let counter = ref 1 in
   for i = 1 to n
   do counter := !counter * i
@@ -32,10 +36,11 @@ let fun_to_test f name n =
 
 
 let tests =
-    [ fun_to_test factorial_1 "factorial_1" 1_000_000
-    ; fun_to_test factorial_2 "factorial_2" 1_000_000
-    ; fun_to_test factorial_3 "factorial_3" 1_000_000
-    ; fun_to_test factorial_4 "factorial_4" 1_000_000
+    [ fun_to_test factorial_1 "factorial_1" 1_000
+    ; fun_to_test factorial_2 "factorial_2" 1_000
+    ; fun_to_test factorial_3 "factorial_3" 1_000
+    ; fun_to_test factorial_4 "factorial_4" 1_000
+    ; fun_to_test factorial_5 "factorial_5" 1_000
     ]
 
 ;;
